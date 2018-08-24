0975d1ad1590e247a4e9d4e6957666f7a76146f9
Felix Carrasco <felix.marcial@gmail.com>
2018-08-24 11:33:18 -0300

    Initial commit

---
+++ enso_mei_rendimiento.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal
+"""
+
+import numpy as np
+import pandas as pd
+
+
+folder = 'c:/Felix/ORA/python_scripts/Test_GITHUB/'
